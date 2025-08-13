1f63cf676d787a0b82dabfb6ca6f6d4740f64246
Mikhael Ravi Medeiros Coelho <mikamed890@gmail.com>
2025-08-13 20:59:34 -0300

    Update App.tsx

--- widget/src/App.tsx
+++ widget/src/App.tsx
@@ -7,7 +7,7 @@ const API_BASE =
   (window as any).__AIWIDGET_API__ ||
   new URLSearchParams(window.location.search).get('api') ||
   process.env.REACT_APP_API_BASE ||
-  'http://localhost:3001';
+  'https://msie-possibility-daughters-ja.trycloudflare.com  ';
 
 const SITE_KEY =
   (window as any).__AIWIDGET_SITE_KEY__ ||
